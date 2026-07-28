06bc9c043ca8d620fbd978a48af1afd14e5cb871
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-07-28 18:59:26 +0200

    Bump USB pipeline to version 38

--- docs/src/runtime/mode-loader.js
+++ docs/src/runtime/mode-loader.js
@@ -1,4 +1,4 @@
-const VERSION = '20260728-37';
+const VERSION = '20260728-38';
 const params = new URLSearchParams(window.location.search);
 const mobileMode = params.get('mode') === 'capture' || params.get('mode') === 'mobile';
 
